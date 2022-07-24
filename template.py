6066a31aff3fb1221d733d45598d7c0c0e47c451
Valentin Shorin <Mesedalg@gmail.com>
2022-07-24 14:45:57 +0300

    create new solutions file template for Pycharm

---
+++ template.py
@@ -0,0 +1,35 @@
+"""
+Title:  ${Title}
+Link: ${Link}
+Grade: ${Grade}
+Tags: ${Tags}
+"""
+from typing import List
+
+input1 = []
+output1 =[]
+
+
+# input2 =
+# output2 =
+# input3 =
+# output3 =
+# input4 =
+# output4 =
+
+class Solution:
+    def some_problem(self):
+        return
+
+
+self = Solution()
+result1 = Solution.some_problem(self, input1)
+assert result1 == output1, print('Result', result1, 'Expected', output1)
+# result2 = Solution.some_problem(self, input2)
+# assert result2 == output1, print('Result', result2, 'Expected', output2)
+# result3 = Solution.some_problem(self, input3)
+# assert result3 == output1, print('Result', result3, 'Expected', output3)
+# result4 = Solution.some_problem(self, input4)
+# assert result4 == output1, print('Result', result4, 'Expected', output4)
+
+print('All done!')
